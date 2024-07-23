2cb4c1cb7baabefe2a1cdacfc50afcd26d0b47cc
EC2 Default User <ec2-user@ip-10-16-10-55.us-west-2.compute.internal>
2024-07-23 16:45:35 +0000

    Lab13

---
+++ net-charge.py
@@ -0,0 +1,38 @@
+# Python3.6  
+# Coding: utf-8  
+
+# Store the human preproinsulin sequence in a variable called preproinsulin
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"  
+
+# Store the remaining sequence elements of human insulin in variables
+lsInsulin = "malwmrllpllallalwgpdpaaa"  
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"  
+aInsulin = "giveqcctsicslyqlenycn"  
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"  
+
+# Construct the insulin sequence
+insulin = bInsulin + aInsulin
+
+# pKa values for amino acids
+pKR = {
+    'y': 10.07,
+    'c': 8.18,
+    'k': 10.53,
+    'h': 6.00,
+    'r': 12.48,
+    'd': 3.65,
+    'e': 4.25
+}
+
+# Count of amino acids in insulin sequence
+seqCount = ({x: float(insulin.count(x)) for x in ['y', 'c', 'k', 'h', 'r', 'd', 'e']})
+
+# Calculate net charge for pH from 0 to 14
+pH = 0
+while pH <= 14:
+    netCharge = (
+        +(sum({x: ((seqCount[x] * (10 ** pKR[x])) / ((10 ** pH) + (10 ** pKR[x]))) for x in ['k', 'h', 'r']}.values()))
+        -(sum({x: ((seqCount[x] * (10 ** pH)) / ((10 ** pH) + (10 ** pKR[x]))) for x in ['y', 'c', 'd', 'e']}.values()))
+    )
+    print('{0:.2f}'.format(pH), netCharge)
+    pH += 1
